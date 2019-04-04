a2099aa2c5ba01d31e36b7f64cafe39baf6c4580
Peter Jenkin <peternjenkin@gmail.com>
2019-04-04 14:07:26 +0100

    6-37 Controls added to View and constraints set (mostly by point & click, and automated resolve)

--- Birthday Note Taker/Birthday Note Taker/ViewController.swift
+++ Birthday Note Taker/Birthday Note Taker/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    @IBOutlet weak var storedNameLabel: UILabel!
+    @IBOutlet weak var storedDateLabel: UILabel!
+    
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func deleteButtonClicked(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
