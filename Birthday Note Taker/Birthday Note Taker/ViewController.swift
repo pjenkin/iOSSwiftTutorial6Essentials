dd19c22483ae4fe2ee2e83bcf2adcf8bf19a8d60
Peter Jenkin <peternjenkin@gmail.com>
2019-04-04 15:49:28 +0100

    6-38 UserDefaults with removeObject to delete persisted app data

--- Birthday Note Taker/Birthday Note Taker/ViewController.swift
+++ Birthday Note Taker/Birthday Note Taker/ViewController.swift
@@ -28,6 +28,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func deleteButtonClicked(_ sender: Any) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        // retrieve persisted UserDefaults data (name & birthday)
+        
+        if let _ = storedName as? String {
+            // if possible to create storedName, cast to String, from (hopefully non-nil) value retrieved...
+            UserDefaults.standard.removeObject(forKey: "name")
+            UserDefaults.standard.synchronize()
+            storedNameLabel.text = "Stored name: "
+        }
+        
+        if let _ = storedDate as? String {      // underscore with let to avoid warning, compiler understands as temporary constant (!)
+            // if possible to create storedName, cast to String, from (hopefully non-nil) value retrieved...
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            UserDefaults.standard.synchronize()
+            storedDateLabel.text = "Stored date: "
+        }
+
     }
     
     override func viewDidLoad() {
